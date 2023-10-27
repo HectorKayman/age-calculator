eeec80c8be2f5b0a32f6bff54749a253f8a885f2
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-27 21:30:00 +0530

    Add functionality to get and set form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,66 @@
+import { useState } from 'react';
 import iconArrow from './assets/images/icon-arrow.svg';
 
 export default function App() {
+  const [date, setDate] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
   return (
     <div className="container">
-      <Form />
+      <Form
+        date={date}
+        month={month}
+        year={year}
+        onChangeDate={setDate}
+        onChangeMonth={setMonth}
+        onChangeYear={setYear}
+      />
       <Display />
     </div>
   );
 }
 
-function Form() {
+function Form({
+  date,
+  month,
+  year,
+  onChangeDate,
+  onChangeMonth,
+  onChangeYear,
+}) {
   return (
     <form>
       <div className="flex-flow form-top">
         <div className="input-div">
           <label htmlFor="form-day">Day</label>
-          <input type="text" id="form-day" placeholder="DD" />
+          <input
+            type="text"
+            id="form-day"
+            placeholder="DD"
+            value={date}
+            onChange={(e) => onChangeDate(e.target.value)}
+          />
         </div>
         <div className="input-div">
           <label htmlFor="form-month">Month</label>
-          <input type="text" id="form-month" placeholder="MM" />
+          <input
+            type="text"
+            id="form-month"
+            placeholder="MM"
+            value={month}
+            onChange={(e) => onChangeMonth(e.target.value)}
+          />
         </div>
         <div className="input-div">
           <label htmlFor="form-year">Year</label>
-          <input type="text" id="form-year" placeholder="YYYY" />
+          <input
+            type="text"
+            id="form-year"
+            placeholder="YYYY"
+            value={year}
+            onChange={(e) => onChangeYear(e.target.value)}
+          />
         </div>
         <div className="input-div"></div>
       </div>
