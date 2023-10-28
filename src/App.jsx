2708a95440ce295e3025976bb01ad9ed3cf34baf
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-10-28 20:01:47 +0530

    Add form custom validation and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ export default function App() {
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
 
+  const [gapDay, setGapDay] = useState(null);
+  const [gapMonth, setGapMonth] = useState(null);
+  const [gapYear, setGapYear] = useState(null);
+
+  function handleCalculateAge() {
+    // Todo
+  }
+
   return (
     <div className="container">
       <Form
@@ -15,39 +23,86 @@ export default function App() {
         onChangeDay={setDay}
         onChangeMonth={setMonth}
         onChangeYear={setYear}
+        calculateAge={handleCalculateAge}
       />
-      <Display />
+      <Display gapDay={gapDay} gapMonth={gapMonth} gapYear={gapYear} />
     </div>
   );
 }
 
-function Form({ day, month, year, onChangeDay, onChangeMonth, onChangeYear }) {
+function Form({
+  day,
+  month,
+  year,
+  onChangeDay,
+  onChangeMonth,
+  onChangeYear,
+  calculateAge,
+}) {
   const [isValidDay, setIsValidDay] = useState(true);
   const [isValidMonth, setIsValidMonth] = useState(true);
   const [isValidYear, setIsValidYear] = useState(true);
+
   const [errorMsgDay, setErrorMsgDay] = useState('');
   const [errorMsgMonth, setErrorMsgMonth] = useState('');
   const [errorMsgYear, setErrorMsgYear] = useState('');
+
+  const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   function handleError(type, fnValid, fnErrMsg, input, min, max) {
     if (input === '') {
       fnValid(false);
       fnErrMsg('This field is required');
+      return false;
     } else if (input < min || input > max || isNaN(Number(input))) {
       fnValid(false);
       fnErrMsg(`Must be a valid ${type}`);
+      return false;
     } else {
       fnValid(true);
       fnErrMsg('');
+      return true;
     }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    handleError('day', setIsValidDay, setErrorMsgDay, day, 1, 31);
-    handleError('month', setIsValidMonth, setErrorMsgMonth, month, 1, 12);
-    handleError('year', setIsValidYear, setErrorMsgYear, year, 1889, 2023);
+    const validDay = handleError(
+      'day',
+      setIsValidDay,
+      setErrorMsgDay,
+      day,
+      1,
+      31
+    );
+    const validMonth = handleError(
+      'month',
+      setIsValidMonth,
+      setErrorMsgMonth,
+      month,
+      1,
+      12
+    );
+    const validYear = handleError(
+      'year',
+      setIsValidYear,
+      setErrorMsgYear,
+      year,
+      1889,
+      2023
+    );
+
+    if (day > monthLength[month - 1]) {
+      setIsValidDay(false);
+      setErrorMsgDay('Must be a valid date');
+      setIsValidMonth(false);
+      setErrorMsgMonth('');
+      setIsValidYear(false);
+      setErrorMsgYear('');
+    } else if (validDay && validMonth && validYear) {
+      calculateAge();
+    }
   }
 
   return (
